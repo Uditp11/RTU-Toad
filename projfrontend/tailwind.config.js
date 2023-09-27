e6ef56926338348f1b5252360860841b7fcbfb90
Uditp11 <88726989+Uditp11@users.noreply.github.com>
2023-09-27 23:03:54 +0530

    t2

--- projfrontend/tailwind.config.js
+++ projfrontend/tailwind.config.js
@@ -1,5 +1,5 @@
 const colors = require('tailwindcss/colors');
-//tailwind css given as colors
+
 module.exports = {
 	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
 	darkMode: false, // or 'media' or 'class'
